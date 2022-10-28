8431d3bc467f0de5c75a0735cff1c9ce67f5c698
Hector Gonzalez <gorozco.hector@gmail.com>
2022-10-28 10:50:41 -0500

    Add component button to upload images

---
+++ src/components/ImageUpload.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { func } from 'prop-types';
+
+const UploadArea = styled.div`
+  & > .inputfile {
+    width: 0.1px;
+    height: 0.1px;
+    opacity: 0;
+    overflow: hidden;
+    position: absolute;
+    z-index: -1;
+  }
+  .App & > .inputfile + label {
+    font-size: 1.25rem;
+    font-weight: 500;
+    border-radius: 1.5rem;
+    padding: 5px 15px 5px 15px;
+    color: var(--reddit-blue);
+    border: 2px solid var(--reddit-blue);
+    background-color: white;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  & > .inputfile:focus + label,
+  & > .inputfile + label:hover {
+    background-color: var(--reddit-blue-super-light)
+  }
+`;
+
+function ImageUpload({ onChange }) {
+  return (
+    <UploadArea>
+      <input
+        type="file"
+        name="file"
+        id="file"
+        className="inputfile"
+        onChange={onChange}
+        accept="image/png, image/jpeg"
+      />
+      <label htmlFor="file" className='image-input'>Choose a file</label>
+    </UploadArea>
+  );
+}
+
+ImageUpload.propTypes = {
+  onChange: func.isRequired,
+}
+
+export default ImageUpload;
