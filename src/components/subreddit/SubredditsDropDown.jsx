3eb9f3dcd37050d854defba27c90ab86a5c17c6f
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-04 16:08:49 -0600

    Fixed bug in subreddits dropdown

--- src/components/subreddit/SubredditsDropDown.jsx
+++ src/components/subreddit/SubredditsDropDown.jsx
@@ -17,57 +17,61 @@ function SubredditsDropDown({ dropdownVisible, toggleDropdown, chooseFromDropdow
       testid="subreddits-dropdown"
     >
       <h1>Your communities</h1>
-      { user.subreddits !== undefined ? 
-      user.subreddits.map((subreddit) => (
-        <React.Fragment key={`${subreddits[subreddit].name}-fragment`}>
-          <hr key={`${subreddits[subreddit].name}-line`} />
-          <a
-            key={`${subreddits[subreddit].name}-link`}
-            onClick={chooseFromDropdown}
-            data={subreddits[subreddit].name}
-          >
-            <img
-              key={`${subreddits[subreddit].name}-icon`}
-              src={subreddits[subreddit].icon}
-              alt=""
+      {user.subreddits !== undefined
+        ? user.subreddits.map((subreddit) => {
+          if (subreddits[subreddit] !== undefined) {
+          <React.Fragment key={`${subreddits[subreddit].name}-fragment`}>
+            <hr key={`${subreddits[subreddit].name}-line`} />
+            <a
+              key={`${subreddits[subreddit].name}-link`}
+              onClick={chooseFromDropdown}
               data={subreddits[subreddit].name}
-              className="user-icon"
-            />
-            <p key={`${subreddits[subreddit].name}-name`} data={subreddits[subreddit].name}>
-              {subreddits[subreddit].name}
-            </p>
-          </a>
-        </React.Fragment>
-      ))
-      :null}
+            >
+              <img
+                key={`${subreddits[subreddit].name}-icon`}
+                src={subreddits[subreddit].icon}
+                alt=""
+                data={subreddits[subreddit].name}
+                className="user-icon"
+              />
+              <p
+                key={`${subreddits[subreddit].name}-name`}
+                data={subreddits[subreddit].name}
+              >
+                {subreddits[subreddit].name}
+              </p>
+            </a>
+          </React.Fragment>
+          }}
+          )
+        : null}
       <hr />
-      {header ?
-      <>
-      <h1>All communities</h1>
-      {Object.values(subreddits).map((subreddit) => (
-        <React.Fragment key={`${subreddit.name}-fragment`}>
-          <hr key={`${subreddit.name}-line`} />
-          <a
-            key={`${subreddit.name}-link`}
-            onClick={chooseFromDropdown}
-            data={subreddit.name}
-          >
-            <img
-              key={`${subreddit.name}-icon`}
-              src={subreddit.icon}
-              alt=""
-              data={subreddit.name}
-              className="user-icon"
-            />
-            <p key={`${subreddit.name}-name`} data={subreddit.name}>
-              {subreddit.name}
-            </p>
-          </a>
-
-        </React.Fragment>
-      ))}
-      </>
-      : null}
+      {header ? (
+        <>
+          <h1>All communities</h1>
+          {Object.values(subreddits).map((subreddit) => (
+            <React.Fragment key={`${subreddit.name}-fragment`}>
+              <hr key={`${subreddit.name}-line`} />
+              <a
+                key={`${subreddit.name}-link`}
+                onClick={chooseFromDropdown}
+                data={subreddit.name}
+              >
+                <img
+                  key={`${subreddit.name}-icon`}
+                  src={subreddit.icon}
+                  alt=""
+                  data={subreddit.name}
+                  className="user-icon"
+                />
+                <p key={`${subreddit.name}-name`} data={subreddit.name}>
+                  {subreddit.name}
+                </p>
+              </a>
+            </React.Fragment>
+          ))}
+        </>
+      ) : null}
     </DropDown>
   );
 }
@@ -76,11 +80,11 @@ export default SubredditsDropDown;
 
 SubredditsDropDown.defaultProps = {
   header: false,
-}
+};
 
 SubredditsDropDown.propTypes = {
   dropdownVisible: bool.isRequired,
   toggleDropdown: func.isRequired,
   chooseFromDropdown: func.isRequired,
   header: bool,
-}
+};
